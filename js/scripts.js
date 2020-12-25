9915f2204280acb4e9c0ee48b31002b54c80b143
javohir1993 <javohirchegabayev@gmail.com>
2020-12-25 19:21:00 +0500

    Completed with "fragment" function and add delete button

--- js/scripts.js
+++ js/scripts.js
@@ -43,6 +43,8 @@ elForm.addEventListener('submit', function (evt) {
   var RelationValue = elRelation.value;
   var PhoneNumberValue = elPhoneNumber.value;
 
+  elNameInput.focus();
+
   var newObject = {
     name: NameInputValue,
     rela: RelationValue,
@@ -50,17 +52,24 @@ elForm.addEventListener('submit', function (evt) {
   };
 
   elPhoneNumber.classList.remove('is-invalid');
+  if (isNaN(elPhoneNumber.value)) {
+    elPhoneNumber.classList.add('is-invalid');
+    elPhoneNumber.focus();
+    return;
+  }
+
   for(var i = 0; i < contactsArray.length; i++){
     if (contactsArray[i].phone === newObject.phone) {
       elPhoneNumber.classList.add('is-invalid');
       return;
     }
-  }
+  };
 
   contactsArray.push(newObject);
 
   relationArray.push(RelationValue);
   elContactsList.innerHTML = '';
+  elDatalist.innerHTML = '';
 
   for(var relation of relationArray){
     var newOption = createElement('option');
@@ -69,7 +78,9 @@ elForm.addEventListener('submit', function (evt) {
     elDatalist.appendChild(newOption);
   }
 
-  contactsArray.forEach(function (contact) {
+  contactsArray.forEach(function (contact, index) {
+
+    var elItemsFragment = document.createDocumentFragment();
 
     var newContact = createElement('li');
     newContact.setAttribute('class', 'list-group-item contact-item mb-2');
@@ -89,19 +100,71 @@ elForm.addEventListener('submit', function (evt) {
     var newDeleteButton = createElement('button');
     newDeleteButton.type = 'button';
     newDeleteButton.textContent = 'Delete';
+    // newObject.dataset.dataId =
+    newDeleteButton.dataset.id = index;
     newDeleteButton.setAttribute('class', 'delete-button btn btn-sm btn-danger');
 
     newContact.appendChild(newName);
     newContact.appendChild(newRela);
     newContact.appendChild(newPhone);
     newContact.appendChild(newDeleteButton);
+    elItemsFragment.appendChild(newContact);
 
-    elContactsList.appendChild(newContact);
+    elContactsList.appendChild(elItemsFragment);
 
     elNameInput.value = '';
     elRelation.value = '';
     elPhoneNumber.value = '';
-  })
+  });
 
 
-})
+});
+
+elContactsList.addEventListener('click', function (evt) {
+  // if (evt.target.matches('button')) {
+  if (evt.target.matches('.btn')) {
+    contactsArray.splice(evt.target.dataset.id, 1);
+  }
+
+  elContactsList.innerHTML = '';
+
+  contactsArray.forEach(function (contact, index) {
+
+    var elItemsFragment = document.createDocumentFragment();
+
+    var newContact = createElement('li');
+    newContact.setAttribute('class', 'list-group-item contact-item mb-2');
+
+    var newName = createElement('h3');
+    newName.setAttribute('class', 'h5 text-truncate');
+    newName.textContent = contact.name;
+
+    var newRela = createElement('p');
+    newRela.setAttribute('class', 'small mb-1');
+    newRela.textContent = contact.rela;
+
+    var newPhone = createElement('a');
+    newPhone.textContent = contact.phone;
+    newPhone.setAttribute('href', `tel:${contact.phone}`);
+
+    var newDeleteButton = createElement('button');
+    newDeleteButton.type = 'button';
+    newDeleteButton.textContent = 'Delete';
+    // newObject.dataset.dataId =
+    newDeleteButton.dataset.id = index;
+    newDeleteButton.setAttribute('class', 'delete-button btn btn-sm btn-danger');
+
+    newContact.appendChild(newName);
+    newContact.appendChild(newRela);
+    newContact.appendChild(newPhone);
+    newContact.appendChild(newDeleteButton);
+    elItemsFragment.appendChild(newContact);
+
+    elContactsList.appendChild(elItemsFragment);
+
+    elNameInput.value = '';
+    elRelation.value = '';
+    elPhoneNumber.value = '';
+  });
+
+});
